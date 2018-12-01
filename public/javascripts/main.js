51bae04273cf5df40619e81da263fe11991f4fb8
CristianAlfaro <00167917@uca.edu.sv>
2018-12-01 15:52:35 -0600

    metodo post listo

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,1 +1,24 @@
-console.log(document.forms.formAsociaciones);
+var form = (document.forms.formAsociaciones);
+form.addEventListener('submit', function(event){
+    event.preventDefault();
+    console.log("hola1");
+    let data= {
+        name: form.name.value,
+        departamento: form.departamento.value,
+        años: form.años.value
+    }
+    console.log(data);
+    fetch('/asociaciones',{
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers:{
+            'Content-Type': 'application/json'
+        }
+    }).then(res => res.json())
+    .then(res => {
+        console.log("guardado en base de datos");
+    })
+    .catch(err => {
+        console.log("algo anda mal")
+    })
+})
