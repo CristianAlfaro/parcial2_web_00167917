5b6b2a527e8ccb9173c94a7115c6c1c2caca577b
CristianAlfaro <00167917@uca.edu.sv>
2018-12-01 17:41:26 -0600

    10 EN EL PARCIAL

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -18,15 +18,16 @@ asociacionController.update = async function (req, res, next){
     let {id} = req.params;
     let asoc= {
         name: req.body.name,
-        departamento: req.body.departamento
+        departamento: req.body.departamento,
+        años: req.body.años
     }
     console.log(asoc);
     try{
         await asociacion.update({_id: id}, asoc);
-        res.status(200).json({message: "asociacion actualizada correctamente"});
+        res.status(200).json({ok:true, message: "asociacion actualizada correctamente"});
     }
     catch(err){
-        return res.status(500).json({err: err, message: "no has insertado correctamente los datos"});
+        return res.status(500).json({ok:true, err: err, message: "no has insertado correctamente los datos"});
     }
 }
 
@@ -37,17 +38,17 @@ asociacionController.create = async function(req,res, next){
     asoc.años = req.body.años;
     try{
         await asoc.save();
-        return res.status(200).json({message: "se guardo con exito"})
+        return res.status(200).json({ok:true, message: "se guardo con exito"})
     }
     catch(err){
-        return res.status(500).json({err: err, message: "no se pudo guardar, revisa los campos"});
+        return res.status(500).json({ok:false, err: err, message: "no se pudo guardar, revisa los campos"});
     }
 }
 
 asociacionController.delete = async function(req, res, next){
     let {id} = req.params;
     await asociacion.remove({_id: id});
-    res.status(200).json({message: "asociacion borrada con exito"});
+    res.status(200).json({ok:true, message: "asociacion borrada con exito"});
 }
 
 module.exports = asociacionController;
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,7 +1,8 @@
+onload();
+
 var form = (document.forms.formAsociaciones);
 form.addEventListener('submit', function(event){
     event.preventDefault();
-    console.log("hola1");
     let data= {
         name: form.name.value,
         departamento: form.departamento.value,
@@ -16,9 +17,86 @@ form.addEventListener('submit', function(event){
         }
     }).then(res => res.json())
     .then(res => {
-        console.log("guardado en base de datos");
+        if(res.ok){
+           onload(); 
+        } else {
+            alert("puede que estes repitiendo nombre o te falten datos");
+        }   
     })
     .catch(err => {
-        console.log("algo anda mal")
+        alert("ocurrio un error en el servidor");
     })
-})
+});
+
+function onload(){
+    fetch('/asociaciones', {
+        method: 'GET'
+    }).then(res => res.json())
+    .then(data => {
+        let filas = "";
+        data.forEach(element => {
+            filas = filas + `
+            <tr>
+                <td class="datos">${element._id}</td>
+                <td class="datos">${element.name}</td>
+                <td class="datos">${element.departamento}</td>
+                <td class="datos">${element.años}</td>
+                <td class="opciones">
+                    <a href="/asociaciones/${element._id}" class="update"><i class="fas fa-pen"></i></a>
+                    <a href="/asociaciones/${element._id}" class="delete"><i class="fas fa-eraser"></i></a>
+                </td>
+            </tr>
+            `
+        });
+        var tabla = document.getElementsByClassName("cuerpoTabla")[0];
+        tabla.innerHTML = filas;
+        //let btn_delete = document.getElementsByClassName("delete");
+        let btn_delete = document.querySelectorAll('.delete');
+        let btn_update = document.querySelectorAll('.update');
+        btn_delete.forEach(element => {
+            element.addEventListener("click", function(event){
+                event.preventDefault();
+                let url = this["href"];
+                fetch(url, {
+                    method: 'DELETE'
+                }).then(res => res.json())
+                .then(res => {
+                    alert("se elimino la asociacion");
+                    onload();
+                }).catch(err => {
+                    alert("Ocurrio un error");
+                })
+            })
+        })
+        btn_update.forEach(element => {
+            element.addEventListener("click", function(event){
+                event.preventDefault();
+                console.log(element.parentNode.parentNode);
+                let url = this["href"];
+                let data= {
+                    name: form.name.value,
+                    departamento: form.departamento.value,
+                    años: form.años.value
+                }
+                console.log(data);
+                fetch(url,{
+                    method: 'PUT',
+                    body: JSON.stringify(data),
+                    headers:{
+                        'Content-Type': 'application/json'
+                    }
+                }).then(res => res.json())
+                .then(res => {
+                    if(res.ok){
+                       onload(); 
+                    } else {
+                        alert("puede que estes repitiendo nombre o te falten datos");
+                    }   
+                })
+                .catch(err => {
+                    alert("ocurrio un error en el servidor");
+                })
+            })
+        })
+    })
+}
